19016f1e37342eb8db69d3351ccaadd3991846ae
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-02 10:28:24 +0800

    完成09.02每日一题

--- Solution.java
+++ Solution.java
@@ -1,21 +1,56 @@
 class Solution {
-    public boolean PredictTheWinner(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n][n];
-        int[][] sum = new int[n][n];
-        for (int i = n-1; i >= 0; i--) {
-            for (int j = n-1; j >= i; j--) {
-                if (j == i) sum[i][j] = nums[i];
-                if (j > i) sum[i][j] = sum[i+1][j] + nums[i];
-            }
+    class Automatation {
+        String START = "start";
+        String SIGNED = "signed";
+        String INTEGER = "integer";
+        String HALFDECIMAL = "halfdecimal";
+        String FALSE = "false";
+        String TRUE = "true";
+        String HALFSCIENCE = "halfscience";
+        String HALFSIGNEDSCIENCE = "halfsignedscience";
+        String DECIMAL = "decimal";
+        String SCIENCE = "science";
+        String POINT = "point";
+        boolean ans = false;
+        String state = START;
+        HashMap<String, String[]> map;
+        public Automatation() {
+            map = new HashMap<>();
+            map.put(START, new String[]{START, POINT, FALSE, INTEGER, SIGNED, FALSE});
+            map.put(POINT, new String[]{FALSE, FALSE, FALSE, DECIMAL, FALSE, FALSE});
+            map.put(SIGNED, new String[]{FALSE, POINT, FALSE, INTEGER, FALSE, FALSE});
+            map.put(INTEGER, new String[]{TRUE, HALFDECIMAL, HALFSCIENCE, INTEGER, FALSE, FALSE});
+            map.put(HALFDECIMAL, new String[]{TRUE, FALSE, HALFSCIENCE, DECIMAL, FALSE, FALSE});
+            map.put(FALSE, new String[]{FALSE, FALSE, FALSE, FALSE, FALSE, FALSE});
+            map.put(TRUE, new String[]{TRUE, FALSE, FALSE, FALSE, FALSE, FALSE});
+            map.put(HALFSCIENCE, new String[]{FALSE, FALSE, FALSE, SCIENCE, HALFSIGNEDSCIENCE, FALSE});
+            map.put(HALFSIGNEDSCIENCE, new String[]{FALSE, FALSE, FALSE, SCIENCE, FALSE, FALSE});
+            map.put(DECIMAL, new String[]{TRUE, FALSE, HALFSCIENCE, DECIMAL, FALSE, FALSE});
+            map.put(SCIENCE, new String[]{TRUE, FALSE, FALSE, SCIENCE, FALSE, FALSE});
         }
-        for (int i = n-1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (j == i) dp[i][j] = nums[i];
-                if (j > i) dp[i][j] = sum[i][j] - Math.min(dp[i+1][j], dp[i][j-1]);
-            }
+        public int getValue(char c) {
+            if (c == ' ') return 0;
+            if (c == '.') return 1;
+            if (c == 'e' || c == 'E') return 2;
+            if (c >= '0' && c <= '9') return 3;
+            if (c == '+' || c == '-') return 4;
+            return 5;
         }
-        if (dp[0][n-1] >= sum[0][n-1] - dp[0][n-1]) return true;
-        return false;
+        public void get(String s) {
+            for (int i = 0; i < s.length(); i++) {
+                int index = getValue(s.charAt(i));
+                state = map.get(state)[index];
+            }
+            if (state == TRUE || state == INTEGER || state == DECIMAL || state == SCIENCE || state == HALFDECIMAL) {
+                ans = true;
+            } else {
+                ans = false;
+            }
+        } 
+    }
+    public boolean isNumber(String s) {
+        Automatation auto = new Automatation();
+        auto.get(s);
+        return auto.ans;
     }
 }
