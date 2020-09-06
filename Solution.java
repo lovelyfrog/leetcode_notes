e39c71826a80d0840487751cc07b62abe61cece5
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-06 17:15:24 +0800

    update on 09.06

--- Solution.java
+++ Solution.java
@@ -1,28 +1,46 @@
 class Solution {
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> ans = new LinkedList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int k = 1;
-        while (queue.size() != 0) {
-            int num = 0;
-            List<Integer> tmpList = new LinkedList<>();
-            while (k != 0) {
-                TreeNode tmp = queue.poll();
-                tmpList.add(tmp.val);
-                if (tmp.left != null) {
-                    queue.offer(tmp.left);
-                    num++;
-                }
-                if (tmp.right != null) {
-                    queue.offer(tmp.right);
-                    num++;
-                }
-                k--;
+    int[][] dist;
+    int[][][] memo;
+    int n;
+    int mod = (int)(1e9+7);
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        n = locations.length;
+        dist = new int[n][n];
+        memo = new int[n][n][fuel+1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Math.abs(locations[i] - locations[j]);
             }
-            k = num;
-            ans.insert(0, tmpList);
         }
-        return ans;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k <= fuel; k++) {
+                    memo[i][j][k] = -1;
+                }
+            }
+        }
+        return helper(start, finish, fuel);
+    }
+    public int helper(int s, int t, int fuel) {
+        if (memo[s][t][fuel] != -1) return memo[s][t][fuel];
+        if (fuel == 0) {
+            if (s == t) {
+                memo[s][t][fuel] = 1;
+                return 1;
+            } else {
+                memo[s][t][fuel] = 0;
+                return 0;
+            }
+        } 
+        int ret = 0;
+        if (s == t) ret++;
+        for (int i = 0; i < n; i++) {
+            if (fuel >= dist[s][i] && i != s) {
+                ret += helper(i, t, fuel-dist[s][i]);
+                ret = ret % mod;
+            }
+        }
+        memo[s][t][fuel] = ret;
+        return ret;
     }
 }
