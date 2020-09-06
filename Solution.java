830d7a92735d0e434a08ef90764dadad7239f45d
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-06 10:49:21 +0800

    完成09.06每日一题

--- Solution.java
+++ Solution.java
@@ -1,33 +1,28 @@
 class Solution {
-    StringBuffer sb = new StringBuffer();
-    HashMap<Integer, Integer> map = new HashMap<>();
-    int[] factorial;
-    public String getPermutation(int n, int k) {
-        factorial = new int[n+1];
-        factorial[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            map.put(i, 1);
-            factorial[i] = factorial[i-1] * i;
-        }
-        helper(n, k);
-        return sb.toString();
-    }
-
-    public void helper(int n, int k) {
-        if (n == 0) return;
-        int length = map.size();
-        int subFact = factorial[n-1];
-        int first = (k-1) / subFact + 1;
-        int rest = (k-1) % subFact + 1;
-        for (int i = 1; i <= length; i++) {
-            if (map.get(i) == 1) {
-                first--;
-                if (first == 0) {
-                    sb.append((char)(i+'0'));
-                    map.put(i, 0);
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int k = 1;
+        while (queue.size() != 0) {
+            int num = 0;
+            List<Integer> tmpList = new LinkedList<>();
+            while (k != 0) {
+                TreeNode tmp = queue.poll();
+                tmpList.add(tmp.val);
+                if (tmp.left != null) {
+                    queue.offer(tmp.left);
+                    num++;
                 }
-            } 
+                if (tmp.right != null) {
+                    queue.offer(tmp.right);
+                    num++;
+                }
+                k--;
+            }
+            k = num;
+            ans.insert(0, tmpList);
         }
-        helper(n-1, rest);
+        return ans;
     }
 }
