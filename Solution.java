bbe1dddb2091d9816c0dffc4c448cf439c4d4ba4
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-07 09:53:12 +0800

    update on 09.07

--- Solution.java
+++ Solution.java
@@ -1,46 +1,39 @@
 class Solution {
-    int[][] dist;
-    int[][][] memo;
-    int n;
-    int mod = (int)(1e9+7);
-    public int countRoutes(int[] locations, int start, int finish, int fuel) {
-        n = locations.length;
-        dist = new int[n][n];
-        memo = new int[n][n][fuel+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dist[i][j] = Math.abs(locations[i] - locations[j]);
-            }
+    HashMap<Integer, Integer> map = new HashMap<>();
+    class Node {
+        int value;
+        int number;
+        Node(int value, int number) {
+            this.value = value;
+            this.number = number;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k <= fuel; k++) {
-                    memo[i][j][k] = -1;
-                }
-            }
-        }
-        return helper(start, finish, fuel);
     }
-    public int helper(int s, int t, int fuel) {
-        if (memo[s][t][fuel] != -1) return memo[s][t][fuel];
-        if (fuel == 0) {
-            if (s == t) {
-                memo[s][t][fuel] = 1;
-                return 1;
+    static Comparator<Node> cNode = new Comparator<>() {
+        public int compare(Node p1, Node p2) {
+            return p2.number - p1.number;
+        }
+    }; 
+    public int[] topKFrequent(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
             } else {
-                memo[s][t][fuel] = 0;
-                return 0;
-            }
-        } 
-        int ret = 0;
-        if (s == t) ret++;
-        for (int i = 0; i < n; i++) {
-            if (fuel >= dist[s][i] && i != s) {
-                ret += helper(i, t, fuel-dist[s][i]);
-                ret = ret % mod;
+                map.put(nums[i], map.get(nums[i])+1);
             }
         }
-        memo[s][t][fuel] = ret;
-        return ret;
+        Node[] nodes = new Node[map.size()];
+        Set<Integer> keys = map.keySet();
+        PriorityQueue<Node> heap = new PriorityQueue<>(cNode);
+        int j = 0;
+        for (Integer key: keys) {
+            nodes[j] = new Node(key, map.get(key));
+            heap.offer(nodes[j]);
+            j++;
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = heap.poll().value;
+        }
+        return ans;
     }
 }
