f76a3d92309bb294d290d7201075ac97a76574ff
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-08 09:43:40 +0800

    update on 09.08

--- Solution.java
+++ Solution.java
@@ -1,39 +1,19 @@
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    class Node {
-        int value;
-        int number;
-        Node(int value, int number) {
-            this.value = value;
-            this.number = number;
-        }
-    }
-    static Comparator<Node> cNode = new Comparator<>() {
-        public int compare(Node p1, Node p2) {
-            return p2.number - p1.number;
-        }
-    }; 
-    public int[] topKFrequent(int[] nums, int k) {
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], 1);
-            } else {
-                map.put(nums[i], map.get(nums[i])+1);
-            }
-        }
-        Node[] nodes = new Node[map.size()];
-        Set<Integer> keys = map.keySet();
-        PriorityQueue<Node> heap = new PriorityQueue<>(cNode);
-        int j = 0;
-        for (Integer key: keys) {
-            nodes[j] = new Node(key, map.get(key));
-            heap.offer(nodes[j]);
-            j++;
-        }
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = heap.poll().value;
-        }
+    List<List<Integer>> ans = new LinkedList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> curr = new LinkedList<>();
+        backTrack(curr, 1, n, k);
         return ans;
+    }
+    public void backTrack(List<Integer> curr, int start, int end, int k) {
+        if (k == 0) {
+            ans.add(new LinkedList<Integer>(curr));
+            return;
+        };
+        for (int i = start; i <= end - k + 1; i++) {
+            curr.add(i);
+            backTrack(curr, i+1, end, k-1);
+            curr.remove(curr.size()-1);
+        }
     }
 }
