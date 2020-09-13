94e7b95f721d564e13bed8f7e0cfdef9e7db7b92
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-13 09:54:05 +0800

    update on 09.12

--- Solution.java
+++ Solution.java
@@ -1,19 +1,62 @@
 class Solution {
-    List<List<Integer>> ans = new LinkedList<>();
-    List<Integer> curr = new LinkedList<>();
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        backTrack(1, n, k);
+    List<Character> leaf = new LinkedList<>();
+    List<Integer> num = new LinkedList<>();
+    public int minimumOperations(String leaves) {
+        int i = 0;
+        int n = leaves.length();
+        while (i < n) {
+            int cnt = 0;
+            if (leaves.charAt(i) == 'r') {
+                while (i < n && leaves.charAt(i) == 'r') {
+                    i++;
+                    cnt++;
+                }
+                leaf.add('r');
+                num.add(cnt);
+            } else {
+                while (i < n && leaves.charAt(i) == 'y') {
+                    i++;
+                    cnt++;
+                }
+                leaf.add('y');
+                num.add(cnt);
+            }
+        }
+        if (check(leaf)) return 0;
+        int ans = 0;
+        if (leaf.get(0) != 'r') {
+            leaf.add(0, 'r');
+            num.add(0, 1);
+            num.set(1, num.get(1)-1);
+            ans++;
+        }
+        if (leaf.get(leaf.size()-1) != 'r') {
+            leaf.add('r');
+            num.add(1);
+            num.set(leaf.size()-2, num.get(leaf.size()-2)-1);
+            ans++;
+        }
+        int yNum = 0, rNum = 0;
+        int yMax = 0;
+        for (int j = 1; j < leaf.size()-1; j++) {
+            if (leaf.get(j) == 'y') {
+                yNum += num.get(j);
+                yMax = Math.max(yMax, num.get(j));
+            } else {
+                rNum += num.get(j);
+            }
+        }
+        if (yNum == 0) {
+            ans += rNum;
+        }else {
+            ans += Math.min(rNum, yNum - yMax);
+        }
         return ans;
     }
-    public void backTrack(int p, int n, int numLeft) {
-        if (n == 0 && numLeft == 0) {
-            ans.add(new LinkedList<>(curr));
-            return;
+    public boolean check(List<Character> leaf) {
+        if (leaf.size() == 3) {
+            if (leaf.get(0) == 'r' && leaf.get(1) == 'y' && leaf.get(2) == 'r') return true;
         }
-        if (n <= 0 || numLeft <= 0) return;
-        backTrack(p+1, n, numLeft);
-        curr.add(p);
-        backTrack(p+1, n-p, numLeft-1);
-        curr.remove(curr.size()-1);
-    }
+        return false;
+    }   
 }
