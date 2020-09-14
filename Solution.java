84ac014bde1fd5eb8f4893df63b888265d5c93f2
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-14 08:47:25 +0800

    update on 09.12

--- Solution.java
+++ Solution.java
@@ -1,62 +1,28 @@
 class Solution {
-    List<Character> leaf = new LinkedList<>();
-    List<Integer> num = new LinkedList<>();
-    public int minimumOperations(String leaves) {
-        int i = 0;
-        int n = leaves.length();
-        while (i < n) {
-            int cnt = 0;
-            if (leaves.charAt(i) == 'r') {
-                while (i < n && leaves.charAt(i) == 'r') {
-                    i++;
-                    cnt++;
-                }
-                leaf.add('r');
-                num.add(cnt);
-            } else {
-                while (i < n && leaves.charAt(i) == 'y') {
-                    i++;
-                    cnt++;
-                }
-                leaf.add('y');
-                num.add(cnt);
-            }
-        }
-        if (check(leaf)) return 0;
+    public int breakfastNumber(int[] staple, int[] drinks, int x) {
+        int m = staple.length;
+        int n = drinks.length;
+        int mod = (int)1e9 + 7;
+        Arrays.sort(staple);
+        Arrays.sort(drinks);
         int ans = 0;
-        if (leaf.get(0) != 'r') {
-            leaf.add(0, 'r');
-            num.add(0, 1);
-            num.set(1, num.get(1)-1);
-            ans++;
-        }
-        if (leaf.get(leaf.size()-1) != 'r') {
-            leaf.add('r');
-            num.add(1);
-            num.set(leaf.size()-2, num.get(leaf.size()-2)-1);
-            ans++;
-        }
-        int yNum = 0, rNum = 0;
-        int yMax = 0;
-        for (int j = 1; j < leaf.size()-1; j++) {
-            if (leaf.get(j) == 'y') {
-                yNum += num.get(j);
-                yMax = Math.max(yMax, num.get(j));
-            } else {
-                rNum += num.get(j);
-            }
-        }
-        if (yNum == 0) {
-            ans += rNum;
-        }else {
-            ans += Math.min(rNum, yNum - yMax);
+        int j = n-1;
+        for (int i = 0; i < m; i++) {
+            if (j < 0) break;
+            if (staple[i] > x) break;
+            int index = find(drinks, 0, j, x - staple[i]);
+            j = index - 1;
+            if (j >= 0) ans = (ans + j + 1) % mod;
         }
         return ans;
     }
-    public boolean check(List<Character> leaf) {
-        if (leaf.size() == 3) {
-            if (leaf.get(0) == 'r' && leaf.get(1) == 'y' && leaf.get(2) == 'r') return true;
-        }
-        return false;
-    }   
+    public int find(int[] drinks, int i, int j, int left) {
+        // 找到drinks在i,j中第一个大于left的索引。
+        if (drinks[j] <= left) return j+1;
+        if (drinks[i] > left) return i;
+        int middle = (i + j) / 2;
+        if (drinks[middle] <= left) return find(drinks, middle+1, j, left);
+        return find(drinks, i, middle, left);
+
+    }
 }
