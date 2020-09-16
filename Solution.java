7faf5006aed42bb51795b342a9c5e12d78ae4f81
lovelyfrog <15307110114@fudan.edu.cn>
2020-09-16 09:54:01 +0800

    update on 09.16

--- Solution.java
+++ Solution.java
@@ -1,19 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public int minimumOperations(String leaves) {
-        int n = leaves.length();
-        int[][] dp = new int[n][3];
-        dp[0][0] = (leaves.charAt(0) == 'r')?0: 1;
-        dp[0][1] = (int)1e5;
-        dp[0][2] = (int)1e5;
-        for (int i = 1; i < n; i++) {
-            char c = leaves.charAt(i);
-            dp[i][0] = dp[i-1][0] + cost(c, 'r');
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][1]) + cost(c, 'y');
-            dp[i][2] = Math.min(dp[i-1][1], dp[i-1][2]) + cost(c, 'r');
-        }
-        return dp[n-1][2];
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root; 
     }
-    public int cost(char a, char b) {
-        return (a == b)?0:1;
+    public void helper(TreeNode root) {
+        if (root == null) return;
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+        helper(root.left);
+        helper(root.right);
     }
 }
