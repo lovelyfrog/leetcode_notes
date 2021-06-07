dc664ff18daa6cb93843aa5c2e7adb51738dda31
lovelyfrog <15307110114@fudan.edu.cn>
2021-06-07 17:58:27 +0800

    add 06.07

--- Solution.java
+++ Solution.java
@@ -1,30 +1,35 @@
 class Solution {
-    public int findMaxForm(String[] strs, int m, int n) {
-        int l = strs.length;
-        int[][][] dp = new int[l+1][m+1][n+1];
-        for (int i = 1; i <= l; i++) {
-            String curr = strs[i-1];
-            int a = 0, b = 0;
-            for (int t = 0; t < curr.length(); t++) {
-                if (curr.charAt(t) == '0') {
-                    a++;
-                } else {
-                    b++;
-                }
-            }
+    public int findTargetSumWays(int[] nums, int target) {
+        int l = nums.length;
+        int allSum = 0;
+        for (int i = 0; i < l; i++) {
+            allSum += nums[i] > 0?nums[i]: -nums[i];
+        }
 
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= n; k++) {
-                    int tmp = dp[i-1][j][k];
-                    if (j-a >= 0 && k-b >= 0) {
-                        if (tmp < dp[i-1][j-a][k-b] + 1) {
-                            tmp = dp[i-1][j-a][k-b] + 1;
-                        }
-                    }
-                    dp[i][j][k] = tmp;
+        if (target < -allSum || target > allSum) {
+            return 0;
+        }
+
+
+        int[][] dp = new int[l+1][2*allSum+1];
+        int newTarget = target + allSum;
+        if (nums[0] != 0) {
+            dp[1][0] = 1;
+            dp[1][2*nums[0]] = 1;
+        } else {
+            dp[1][0] = 2;
+        }
+
+
+        for (int i = 2; i <= l; i++) {
+            for (int j = 0; j < 2*allSum+1; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j-2*nums[i-1] >= 0) {
+                    dp[i][j] += + dp[i-1][j-2*nums[i-1]];
                 }
             }
         }
-        return dp[l][m][n];
+
+        return dp[l][newTarget];
     }
 }
