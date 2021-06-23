6bc67213f0907b035efab9de0c25150b2d8866ca
lovelyfrog <15307110114@fudan.edu.cn>
2021-06-23 11:41:26 +0800

    update

--- Solution.java
+++ Solution.java
@@ -1,53 +1,12 @@
-class ThroneInheritance {
-
-    HashMap<String, List<String>> map = new HashMap<>();
-    HashMap<String, Integer> death = new HashMap<>();
-    String kingName;
-    public ThroneInheritance(String kingName) {
-
-        map.put(kingName, new LinkedList<String>());
-        this.kingName = kingName;
-
-    }
-    
-    public void birth(String parentName, String childName) {
-        List<String> childs = map.get(parentName);
-        childs.add(childName);
-        if (!map.containsKey(childName)) {
-            map.put(childName, new LinkedList<String>());    
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            cnt += n & 1;
+            n = n >> 1;
         }
-        // childParent.put(childName, parentName); 
-    }
-    
-    public void death(String name) {
-        // String parent = childParent.get(name);
-        // List<String> childs = map.get(parent);
-        death.put(name, -1);
-
-    }
-    
-    public List<String> getInheritanceOrder() {
-        List<String> currOrder = new LinkedList<>();
-        helper(currOrder, this.kingName);
-        return currOrder;
-    }
-
-    public void helper(List<String> currOrder, String curr) {
-        if (!death.containsKey(curr)) {
-            currOrder.add(curr);
-        }
-        List<String> childs = map.get(curr);
-        for (int i = 0; i < childs.size(); i++) {
-            String child = childs.get(i);
-            helper(currOrder, child);
-        }
+        return cnt;
     }
 }
-
-/**
- * Your ThroneInheritance object will be instantiated and called as such:
- * ThroneInheritance obj = new ThroneInheritance(kingName);
- * obj.birth(parentName,childName);
- * obj.death(name);
- * List<String> param_3 = obj.getInheritanceOrder();
- */
+    
