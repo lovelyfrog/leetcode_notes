a813f0cfbfbadd040c9a6f4a667e3143b7b82682
lovelyfrog <15307110114@fudan.edu.cn>
2021-06-24 20:00:59 +0800

    update

--- Main.java
+++ Main.java
@@ -1,103 +1,39 @@
 import java.util.*;
 public class Main {
 
-    public class MinHeap {
-        int cap;
-        int[] array;
-        int next_pos;
-        public MinHeap(int cap) {
-            this.cap = cap;
-            this.array = new int[cap];
+    public class Solution {
+        public int MOD = (int) 1e9 + 7;
+
+        public boolean isPrime(int number) {
+            if (number == 1) return false;
+            for (int i = 2; i * i <= number; i++) {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
 
-        public MinHeap(int[] array) {
-            this.cap = array.length;
-            this.next_pos = array.length;
-            this.array = new int[array.length];
-            for (int i = 0; i < array.length; i++) {
-                this.array[i] = array[i];
-                upShift(i);
-            }
-
-        }
-
-        public void upShift(int i) {
-            if (i == 0) return;
-            int p = parent(i);
-            if (this.array[p] > this.array[i]) {
-                swap(p, i);
-                upShift(p);
-            }
-            return;
-        }
-
-        public void downShift(int i) {
-            if (i >= next_pos) return;
-            int isLeft = 0;
-            int isRight = 0;
-            if (left(i) < next_pos &&  this.array[left(i)] < this.array[i]) {
-                swap(left(i), i);
-                isLeft = 1;
-            }
-            if (right(i) < next_pos &&  this.array[right(i)] < this.array[i]) {
-                swap(right(i), i);
-                isRight = 1;
-            }
-
-            if (isLeft + isRight == 1) {
-                if (isLeft == 1) {
-                    downShift(left(i));
-                } else {
-                    downShift(right(i));
+        public int countPrimeStrings(String number) {
+            int l = number.length();
+            int[] dp = new int[l+1];
+            dp[l] = 1;
+            for (int i = l-1; i >= 0; i--) {
+                for (int k = 1; k <= 6; k++) {
+                    if (i + k > l) break;
+                    String sub = number.substring(i, i+k);
+                    int num = Integer.valueOf(sub);
+                    if (isPrime(num))
+                        dp[i] += dp[i+k];
+                        dp[i] %= MOD;
                 }
-            } else if (isLeft + isRight == 2) {
-                downShift(right(i));
             }
-
-            return;    
-
+            return dp[0];
         }
-
-        public void swap(int i, int j) {
-            int tmp = this.array[i];
-            this.array[i] = this.array[j];
-            this.array[j] = tmp;
-        }
-
-        public int parent(int i) {
-            return (i - 1) >> 1;
-        }
-
-        public int left(int i) {
-            return (i << 1) + 1;
-        }
-
-        public int right(int i) {
-            return (i << 1) + 2;
-        }
-
-        public int getMin() {
-            return this.array[0];
-        }
-
-        public int delMin() {
-            int x = getMin();
-            swap(--next_pos, 0);
-            downShift(0);
-            return x;
-        }
-
     }
 
     public void test() {
-        int[] nums = {7, 5, 4, 8, 2, 1, 9, 10};
-        MinHeap heap = new MinHeap(nums);
-        
-        for (int i = 0; i < nums.length; i++) {
-            System.out.printf("%d ", heap.delMin());
-            // heap.delMin();
-        }
-        System.out.println();
+        Solution sol = new Solution();
+        System.out.println(sol.countPrimeStrings("11373"));
+        System.out.println(sol.countPrimeStrings("3175"));
     }
 
     public static void main(String[] args) {
--- Solution.java
+++ Solution.java
@@ -1,12 +1,39 @@
-public class Solution {
-    // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
-        int cnt = 0;
-        for (int i = 0; i < 32; i++) {
-            cnt += n & 1;
-            n = n >> 1;
+class Solution {
+    public int maxPoints(int[][] points) {
+        int n = points.length;
+        double[][] g = new double[n][n];
+        if (n == 1) return 1;
+
+        int ans = 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (points[i][0] - points[j][0] == 0) {
+                    g[i][j] = 1e6;
+                    g[j][i] = g[i][j];
+                    continue;
+                }
+                g[i][j] = (double) (points[i][1] - points[j][1]) / (points[i][0] - points[j][0]);
+                g[j][i] = g[i][j];
+
+            }
         }
-        return cnt;
+
+        for (int i = 0; i < n; i++) {
+            HashMap<Double, Integer> map = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue;
+                if (!map.containsKey(g[i][j])) {
+                    map.put(g[i][j], 2);
+                } else {
+                    int tmp = map.get(g[i][j]);
+                    map.put(g[i][j], tmp+1);
+                    if (ans < tmp+1) {
+                        ans = tmp+1;
+                    }
+
+                }
+            }
+        }
+        return ans;
     }
 }
-    
